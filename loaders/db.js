761e7e0c91b55093f5612ba18641a90d6fc623dc
Abhisheksingh <71022771+mindfreak01@users.noreply.github.com>
2020-09-10 00:56:15 +0530

    Update db.js

--- loaders/db.js
+++ loaders/db.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const config = require("config");
 
-const db_name = "task-api"
-const db_pass = "Sumanaman@2020"
+const db_name = config.get("Secrets.db_name");
+const db_pass = config.get("Secrets.db_pass");
 const url = `mongodb+srv://abhishek:${db_pass}@cluster0.rqshu.mongodb.net/${db_name}?retryWrites=true&w=majority`;
 
 module.exports = () => {
