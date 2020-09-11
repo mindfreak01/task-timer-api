9a80fdc8052f5d6bcf2d5cedcdd310387b5b5a91
mindfreak01 <71022771+mindfreak01@users.noreply.github.com>
2020-09-11 23:04:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const config = require("config");
 require("./loaders/db")();
 require("./loaders/routes")(app);
 
-const port = config.get("PORT") || 5000;
+//const port = config.get("PORT") || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server listening at port:${port}`));
